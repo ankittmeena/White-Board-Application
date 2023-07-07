afae11b8c8e9301a383b014ac676508e8ec2be46
Ankit Kumar Meena <ankit.k.meena2301@gmail.com>
2023-07-07 18:16:45 +0530

    Add files via upload

---
+++ White Board.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+root=Tk()
+root.title("White Board")
+root.geometry("1050x570+150+50")
+root.config(bg="#f2f3f5")
+root.resizable(False, False)
+
+root.mainloop()
